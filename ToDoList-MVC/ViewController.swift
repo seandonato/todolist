c3106ebdb24c3421a78ed7404638bb646a1c9eb8
Sean Donato <seanvincentdonato@gmail.com>
2023-03-10 14:33:52 -0500

    Initial Commit

---
+++ ToDoList-MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList-MVC
+//
+//  Created by Sean Donato on 3/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
