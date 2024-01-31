a5e62caf75712c5732d6913f9cf0fb9e25b4d129
Sean Donato <seanvincentdonato@gmail.com>
2024-01-31 00:44:18 -0500

    added status block animation

--- ToDoList-MVC/StatusPicker.swift
+++ ToDoList-MVC/StatusPicker.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class StatusPicker: UIView {
+class StatusPicker: UIView,UIGestureRecognizerDelegate {
 
     /*
     // Only override draw() if you perform custom drawing.
@@ -46,12 +46,13 @@ class StatusPicker: UIView {
         super.init(frame: frame)
         setupView()
     }
-    
+    var currentView = UIView()
     func setupView(){
         stack.addArrangedSubview(readyLabel)
         stack.addArrangedSubview(inProgressLabel)
         stack.addArrangedSubview(blockedLabel)
         stack.addArrangedSubview(doneLabel)
+        
         self.addSubview(stack)
         stack.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -67,7 +68,13 @@ class StatusPicker: UIView {
             yellowView.widthAnchor.constraint(equalToConstant: readyLabel.frame.width + 16),
             yellowView.heightAnchor.constraint(equalToConstant: readyLabel.frame.height)
         ])
+        
         yellowView.backgroundColor = .yellow
+        currentView = yellowView
+        var readyGesture = MyTapGesture(target: self, action: #selector(statusTransition), statusView: yellowView)
+        readyGesture.addTarget(self, action: #selector(statusTransition))
+        readyLabel.addGestureRecognizer(readyGesture)
+        readyGesture.delegate = self
         
         let blueView = UIView()
         blueView.backgroundColor = .blue
@@ -77,6 +84,10 @@ class StatusPicker: UIView {
             blueView.widthAnchor.constraint(equalToConstant: inProgressLabel.frame.width + 16),
             blueView.heightAnchor.constraint(equalToConstant: inProgressLabel.frame.height)
         ])
+        var progGesture = MyTapGesture(target: self, action: #selector(statusTransition), statusView: blueView)
+        progGesture.addTarget(self, action: #selector(statusTransition))
+        inProgressLabel.addGestureRecognizer(progGesture)
+        progGesture.delegate = self
 
         let greenView = UIView()
         greenView.backgroundColor = .green
@@ -86,11 +97,19 @@ class StatusPicker: UIView {
             greenView.widthAnchor.constraint(equalToConstant: doneLabel.frame.width + 16),
             greenView.heightAnchor.constraint(equalToConstant: doneLabel.frame.height)
         ])
+        var doneGesture = MyTapGesture(target: self, action: #selector(statusTransition), statusView: greenView)
+        doneGesture.addTarget(self, action: #selector(statusTransition))
+        doneLabel.addGestureRecognizer(doneGesture)
+        doneGesture.delegate = self
 
 
         let redView = UIView()
         redView.backgroundColor = .red
         redView.translatesAutoresizingMaskIntoConstraints = false
+        var blockedGesture = MyTapGesture(target: self, action: #selector(statusTransition), statusView: redView)
+        blockedGesture.addTarget(self, action: #selector(statusTransition))
+        blockedLabel.addGestureRecognizer(blockedGesture)
+        blockedGesture.delegate = self
 
         NSLayoutConstraint.activate([
             redView.widthAnchor.constraint(equalToConstant: blockedLabel.frame.width + 16),
@@ -109,9 +128,34 @@ class StatusPicker: UIView {
             statusStack.topAnchor.constraint(equalTo: stack.bottomAnchor,constant: 16),
             statusStack.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 8)
         ])
-
+        readyLabel.isUserInteractionEnabled = true
+        inProgressLabel.isUserInteractionEnabled = true
+        doneLabel.isUserInteractionEnabled = true
+        blockedLabel.isUserInteractionEnabled = true
+        redView.alpha = 0.0
+        blueView.alpha = 0.0
+        greenView.alpha = 0.0
+        stack.isUserInteractionEnabled = true
+    }
+    
+    @objc func statusTransition(sender:MyTapGesture){
+        if currentView != sender.statusView
+        {
+            UIView.animate(withDuration: 1.5) {
+                self.currentView.alpha = 0.0
+                sender.statusView.alpha = 1.0
+                self.currentView = sender.statusView
+            }
+        }
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+class MyTapGesture: UITapGestureRecognizer {
+    var statusView : UIView
+    init(target: Any?, action: Selector?,statusView:UIView) {
+        self.statusView = statusView
+        super.init(target: target, action: action)
     }
 }
